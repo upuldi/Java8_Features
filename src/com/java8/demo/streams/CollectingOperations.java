009646c8636a9f3620dcf8bbdf60932ab2368bbe
UpulDoluweera <upul.doluweera@btfinancialgroup.com>
2017-02-13 16:27:54 +1100

    updated

--- src/com/java8/demo/streams/CollectingOperations.java
+++ src/com/java8/demo/streams/CollectingOperations.java
@@ -3,7 +3,9 @@ package com.java8.demo.streams;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -37,6 +39,27 @@ public class CollectingOperations {
         Map<Integer,String> stringLengthValueMap = collectToMapStream.collect(Collectors
                 .toMap(s -> s.length(), s -> s));
         System.out.println(stringLengthValueMap);
+
+
+        /**When you are collecting from a primitive stream you have to use the function boxed() to convert the stream
+         * into a wrapper stream to be collected by the List
+         */
+
+        List<Integer> integerList = IntStream.rangeClosed(1,100)
+                .map(i -> i % 10)
+                .boxed() //ToConvert to wrapper stream
+                .collect(Collectors.toList());
+
+        Map<Integer, Integer> integerSet = IntStream.rangeClosed(1, 100)
+                .map(i -> i % 10)
+                .filter(i -> i % 3 == 0 ? true : false)
+                .map(i -> i * i)
+                .distinct()
+                .sorted()
+                .limit(5)
+                .boxed() //Function Identity can be used when a function returns same parameter it receives.
+                .collect(Collectors.toMap( Function.identity()  , Function.identity() ));
+
 
     }
 
